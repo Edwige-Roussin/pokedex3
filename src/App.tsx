97b9b5af8f6749c53817b9ed52156c8f2e9abfbb
Edwige ROUSSIN <edwige.roussin@gmail.com>
2024-10-27 10:59:26 +0100

    firt commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section>
+      <h1>Hello React</h1>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/MyTitle.tsx
@@ -0,0 +1,5 @@
+function MyTitle() {
+    return <h1>MyTitle</h1>;
+  }
+  
+  export default MyTitle;
